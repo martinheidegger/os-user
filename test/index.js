595bd755bebfbc3385f21d6f6d6e607d2d82c992
Martin Heidegger <martin.heidegger@gmail.com>
2015-12-07 04:26:36 +0900

    Added more detailed tests

--- test/index.js
+++ test/index.js
@@ -1,20 +1,35 @@
 'use strict'
 var tap = require('tap')
-var user = require('../os-user.js')
+var execSync = require('child_process').execSync
 
 if (process.platform === 'win32') {
-  process.env.USERDOMAIN = 'knights'
-  process.env.USERNAME = 'sirUser'
-
   tap.test('basic windows test', function (t) {
-    t.equal(user(), 'knights//sirUser')
-    t.end()
+    process.env.USERDOMAIN = 'knights'
+    process.env.USERNAME = 'sirUser'
+    require('..')(function (err, user) {
+      t.equal(err, null)
+      t.equal(user, 'knights//sirUser')
+      t.end()
+    })
   })
+  // Note: on windows it will always at least return '//' and as an logical result it will never run whoami
 } else {
-  process.env.USER = 'sirUser'
-
   tap.test('basic unix test', function (t) {
-    t.equal(user(), 'sirUser')
-    t.end()
+    process.env.USER = 'sirUser'
+    require('..')(function (err, user) {
+      t.equal(err, null)
+      t.equal(user, 'sirUser')
+      t.end()
+    })
+  })
+
+  tap.test('fallback windows test', function (t) {
+    delete process.env.USER
+    require.cache = {}
+    require('..')(function (err, user) {
+      t.equal(err, null)
+      t.equal(user, execSync('whoami').toString().trim())
+      t.end()
+    })
   })
 }
